83add1aa9aa3d47ed12a3a371d4570a4ca3052e5
Mykyta Paliienko <floyernick@gmail.com>
2018-04-02 23:32:00 +0300

    Add Cocktail Sort

---
+++ CocktailSort/CocktailSort.go
@@ -0,0 +1,20 @@
+package main
+
+func CocktailSort(array []int) {
+	swapCount := 1
+	for swapCount > 0 {
+		swapCount = 0
+		for itemIndex := 0; itemIndex < len(array)-1; itemIndex++ {
+			if array[itemIndex] > array[itemIndex+1] {
+				array[itemIndex], array[itemIndex+1] = array[itemIndex+1], array[itemIndex]
+				swapCount += 1
+			}
+		}
+		for itemIndex := len(array) - 1; itemIndex > 0; itemIndex-- {
+			if array[itemIndex] < array[itemIndex-1] {
+				array[itemIndex], array[itemIndex-1] = array[itemIndex-1], array[itemIndex]
+				swapCount += 1
+			}
+		}
+	}
+}

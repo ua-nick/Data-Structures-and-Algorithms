a1566103225eab7958f3c73db531ae5e25300ee2
Mykyta Paliienko <floyernick@gmail.com>
2018-04-02 23:52:33 +0300

    Add Gnome Sort

---
+++ GnomeSort/GnomeSort.go
@@ -0,0 +1,15 @@
+package main
+
+func GnomeSort(array []int) {
+	itemIndex := 0
+	for itemIndex < len(array)-1 {
+		if array[itemIndex] > array[itemIndex+1] {
+			array[itemIndex], array[itemIndex+1] = array[itemIndex+1], array[itemIndex]
+			if itemIndex != 0 {
+				itemIndex -= 1
+			}
+		} else {
+			itemIndex += 1
+		}
+	}
+}

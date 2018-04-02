a75e137f17cc081c56db63cdfbad63c1cc7b0b57
Mykyta Paliienko <floyernick@gmail.com>
2018-04-02 23:23:24 +0300

    Refactor Bubble Sort

--- BubbleSort/BubbleSort.go
+++ BubbleSort/BubbleSort.go
@@ -4,17 +4,10 @@ func BubbleSort(array []int) {
 	swapCount := 1
 	for swapCount > 0 {
 		swapCount = 0
-		firstItem := 0
-		secondItem := 1
-		for i := 0; i < len(array); i++ {
-			if array[firstItem] > array[secondItem] {
-				array[firstItem], array[secondItem] = array[secondItem], array[firstItem]
+		for itemIndex := 0; itemIndex < len(array)-1; itemIndex++ {
+			if array[itemIndex] > array[itemIndex+1] {
+				array[itemIndex], array[itemIndex+1] = array[itemIndex+1], array[itemIndex]
 				swapCount += 1
-			}
-			firstItem += 1
-			secondItem += 1
-			if secondItem == len(array) {
-				break
 			}
 		}
 	}

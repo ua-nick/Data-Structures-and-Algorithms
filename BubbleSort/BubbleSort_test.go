81c1a0bb1a2c81c6b9635f25cc710120164bc0fc
Mykyta Paliienko <floyernick@gmail.com>
2018-04-02 22:32:51 +0300

    Add tests for algorithms

---
+++ BubbleSort/BubbleSort_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+	"time"
+)
+
+func TestBubbleSort(t *testing.T) {
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	array1 := make([]int, random.Intn(100))
+	for i := range array1 {
+		array1[i] = random.Intn(100)
+	}
+	array2 := make(sort.IntSlice, len(array1))
+	copy(array2, array1)
+	BubbleSort(array1)
+	array2.Sort()
+	for i := range array1 {
+		if array1[i] != array2[i] {
+			t.Fail()
+		}
+	}
+}
---
+++ TernarySearch/TernarySearch_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func SortArray(array []int) {
+	for itemIndex, itemValue := range array {
+		for itemIndex != 0 && array[itemIndex-1] > itemValue {
+			array[itemIndex] = array[itemIndex-1]
+			itemIndex -= 1
+		}
+		array[itemIndex] = itemValue
+	}
+}
+
+func TestTernarySearch(t *testing.T) {
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	array := make([]int, random.Intn(100))
+	for i := range array {
+		array[i] = random.Intn(100)
+	}
+	SortArray(array)
+	for _, value := range array {
+		result := TernarySearch(array, value)
+		if result == -1 {
+			t.Fail()
+		}
+	}
+}

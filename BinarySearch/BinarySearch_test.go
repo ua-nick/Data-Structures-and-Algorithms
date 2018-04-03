560cb1289be771113eb308528b30dc82485ae4bc
Mykyta Paliienko <floyernick@gmail.com>
2018-04-03 23:56:38 +0300

    Fix test array generation

--- BinarySearch/BinarySearch_test.go
+++ BinarySearch/BinarySearch_test.go
@@ -18,7 +18,7 @@ func SortArray(array []int) {
 
 func TestBinarySearch(t *testing.T) {
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
-	array := make([]int, random.Intn(100))
+	array := make([]int, random.Intn(100-10)+10)
 	for i := range array {
 		array[i] = random.Intn(100)
 	}
--- BubbleSort/BubbleSort_test.go
+++ BubbleSort/BubbleSort_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestBubbleSort(t *testing.T) {
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
-	array1 := make([]int, random.Intn(100))
+	array1 := make([]int, random.Intn(100-10)+10)
 	for i := range array1 {
 		array1[i] = random.Intn(100)
 	}

e96d78bdb7e6838485c74e32594e462f2c736afe
Mykyta Paliienko <floyernick@gmail.com>
2018-05-08 09:56:41 +0300

    Fix Naive String Search test

--- NaiveStringSearch/NaiveStringSearch_test.go
+++ NaiveStringSearch/NaiveStringSearch_test.go
@@ -1,7 +1,6 @@
 package NaiveStringSearch
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
 	"time"
@@ -10,13 +9,12 @@ import (
 func TestNaiveStringSearch(t *testing.T) {
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 	letters := []rune("abcdefghijklmnopqrstuvwxyz")
-	text := make([]rune, random.Intn(15-5)+5)
+	text := make([]rune, random.Intn(15-10)+10)
 	for i := range text {
 		text[i] = letters[rand.Intn(len(letters))]
 	}
 	end := random.Intn(len(text)-5) + 5
 	start := random.Intn(end)
-	fmt.Println(len(text), end, start)
 	result := NaiveStringSearch(string(text), string(text[start:end]))
 	if result == -1 {
 		t.Fail()
